64d0adce60881189484f3c40dab959bafb332352
iknite <enrique@iknite.com>
2018-07-09 10:33:33 +0200

    Add Stringer implementation to tree.Node

---
+++ main.go
@@ -0,0 +1,11 @@
+package main
+
+import (
+	"fmt"
+
+	"github.com/iknite/bygone-tree/tree"
+)
+
+func main() {
+	fmt.Print(new(tree.Node))
+}
---
+++ tree/tree.go
@@ -0,0 +1,8 @@
+package tree
+
+type Node struct{}
+
+// Stringer implementation
+func (n *Node) String() string {
+	return "I'm a node\n"
+}
